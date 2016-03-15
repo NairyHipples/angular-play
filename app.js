59c13b7f4979e863af35588ae88bd215d2ed9188
Jonatan Östling <jonatan.ostling@gmail.com>
2016-03-15 23:36:46 +0100

    route test

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-var demoApp = angular.module('demoApp', []);
+var demoApp = angular.module('demoApp', ['ngRoute']);
+demoApp.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+
+  $routeProvider
+    .when('/', {
+      controller: 'SimpleController',
+      templateUrl: 'view1.html'
+    })
+    .when('/partial2', {
+      controller: 'SimpleController',
+      templateUrl: 'view2.html'
+    })
+    .otherwise({ redirectTo: '/' });
+}]);
 
 var controllers = {};
 controllers.SimpleController = function($scope) {
@@ -8,6 +22,13 @@ controllers.SimpleController = function($scope) {
     { name: 'Heedy Wahlin', city: 'Chandler' },
     { name: 'Thomas Winter', city: 'Seattle' }
   ];
+  $scope.addCustomer = function() {
+    $scope.customers.push(
+      {
+          name: $scope.newCustomer.name,
+          city: $scope.newCustomer.city
+      });
+  };
 };
 
 demoApp.controller(controllers);
